2a69d22aea748c1ba8d51fbfa7923a39966f47d4
Gedeon Ntwali <ntwari28@gmail.com>
2017-01-19 12:30:36 +0200

    I added a model called Product

---
+++ db/migrate/20170119102928_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :type
+      t.string :price
+      t.string :size
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
